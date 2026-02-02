d8877e06b3d3b91da2f0df5d513cc7ddce254cb6
bhumikhurana9 <bhumikhurana9@gmail.com>
2026-02-02 13:34:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode, command }) => ({
+  base:
+    command === "build"
+      ? process.env.BASE_PATH || "/"
+      : "/",
   server: {
     host: "::",
     port: 8080,
